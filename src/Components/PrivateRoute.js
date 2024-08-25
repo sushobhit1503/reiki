c87913541511d6b5cd3a14e79db1f3042d01b0ac
Sushobhit Srivastava <sushobhitofficial@gmail.com>
2024-08-26 01:53:37 +0530

    Removed login page

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 function PrivateRoute({ children }) {
     const auth = localStorage.getItem("uid");
-    return auth ? <>{children}</> : <Navigate to="/login" />;
+    return auth ? <>{children}</> : <Navigate to="/" />;
 }
 
 export default PrivateRoute
